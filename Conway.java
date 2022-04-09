84a587ecdc33226dcde422d71ca05f6ede12b39e
Hetul3 <97708864+Hetul3@users.noreply.github.com>
2022-04-08 20:33:17 -0400

    Add files via upload

---
+++ Conway.java
@@ -0,0 +1,120 @@
+package tictactoe;
+
+import java.awt.Color;
+import java.util.Random;
+
+import hsa2.GraphicsConsole;
+
+public class Conway {
+
+	public static void main(String[] args) {
+		
+		new Conway();
+		
+	}
+	
+	static final int SIZE = 5;
+	int height = 700;
+	int width = 700;
+	int col = width/SIZE;
+	int row = height/SIZE;
+	boolean boarding = true;
+	boolean running = true;
+	
+	Random rand = new Random();
+	
+	int[][] board = new int[col][row];
+	
+	GraphicsConsole gc = new GraphicsConsole(width, height);
+	
+	Conway() {
+		setupGraphics();
+		initialBoard();
+		while(running) {
+			handleclicks();
+			draw();
+		}
+	}
+
+
+
+	private void handleclicks() {
+		int mx = gc.getMouseX();
+		int my = gc.getMouseY();
+		int cols = mx/col;
+		int rows = my/row;
+
+		
+	}
+
+	int gameRule(int status, int neighbours) {
+		if(status == 1 && neighbours > 3) return (0);
+		else if(status == 1 && neighbours < 2) return (0);
+		else if (status == 0 && neighbours == 3) return(1);
+		else return(status);
+	}
+	
+	int countNeighbours(int x, int y) {
+		int neighbours = 0;
+	    for (int i=-1; i <= 1; i++) {
+	      for (int j=-1; j <= 1; j++) {
+	        neighbours += board[x+j][y+i];
+	      }
+	    }
+	    neighbours -= board[x][y];
+	    return (neighbours);
+		
+	}
+
+	void initialBoard() {
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < col; j++) {
+				int r = rand.nextInt(2);
+				board[j][i] = r;
+			}
+		}
+	}
+	
+	void drawGraphics(int [][]board) {
+		width = gc.getDrawWidth();
+		height = gc.getDrawHeight();
+		
+		synchronized(gc) {
+			for(int y = 0; y < row; y++) {
+				for(int x = 0; x < col; x++) {
+					if(board[x][y] == 0) {
+						gc.setColor(Color.BLACK);
+						gc.fillRect(x*SIZE, y*SIZE, SIZE, SIZE);
+					}
+					else if(board[x][y] == 1) {
+						gc.setColor(Color.WHITE);
+						gc.fillRect(x*SIZE, y*SIZE, SIZE, SIZE);
+					}
+				}
+			}
+		}
+	}
+	
+	void draw() {
+		int[][] board2 = new int[col][row];
+		for(int i = 1; i < row-1; i++) {
+			for(int j = 1; j < col - 1; j++) {
+				int neighbours = countNeighbours(i, j);
+				board2[i][j] = gameRule(board[i][j], neighbours);
+			}
+		}
+		board = board2;
+		drawGraphics(board);
+		gc.sleep(25);
+		draw();
+	}
+	
+	void setupGraphics() {
+		gc.setTitle("Conway's Game of Life");
+		gc.setAntiAlias(true);
+		gc.setStroke(2);
+		gc.setLocationRelativeTo(null);
+		gc.enableMouse();
+	}
+	
+}
